bd1d01eaee0f18b04de277c376e4b0d26f3ee97f
Warda-99 <82954272+Warda-99@users.noreply.github.com>
2023-04-25 22:14:45 +0200

    x

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from 'axios'
 import { Navigate, useNavigate } from "react-router-dom";
 
 export const Login = (props) => {
@@ -8,10 +9,39 @@ export const Login = (props) => {
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
+    //http://46.239.134.230:8080/api/test
     e.preventDefault();
-    console.log(email);
+    //console.log(email);
 
-    navigate('/home', { replace: true })
+    //navigate('/home', { replace: true })
+
+    const user = { email, pass }
+		axios
+			.post('http://46.239.134.230:8080/api/login', user)
+			.then((res) => {
+				// localStorage.setItem('token', res.data.accessToken)
+				// var value = res.headers['set-cookie']
+				// console.log(value)
+				// console.log(res.data)
+				// setUser('')
+				// setpassword('')
+				// setSuccess(true)
+				// localStorage.setItem('user', JSON.stringify(res.data))
+        console.log(res)
+			})
+			.catch((err) => {
+				// if (err.response) {
+				// 	setErrMsg(`${err.response.data.error}`)
+				// } else if (err.response.status === 400) {
+				// 	setErrMsg(`${err.response.data.error}`)
+				// } else if (err.response.status === 401) {
+				// 	setErrMsg(`${err.response.data.error}`)
+				// } else {
+				// 	setErrMsg(`${err.response.data.error}`)
+				// }
+				// errRef.current.focus()
+				console.log(err)
+			})
   };
 
   return (
