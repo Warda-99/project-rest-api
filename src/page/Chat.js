d9e4152242a7ad5fa1e0becb89175921dd50a908
Warda-99 <82954272+Warda-99@users.noreply.github.com>
2023-06-21 23:58:40 +0200

    show

--- src/page/Chat.js
+++ src/page/Chat.js
@@ -1,68 +1,104 @@
-import { useEffect, useRef, useState } from "react";
-import TopBar from "./TopBar";
-import "../styles/Chat.css";
+// import { useEffect, useRef, useState } from "react";
+// import TopBar from "./TopBar";
+// import "../styles/Chat.css";
+
+// export const Chat = () => {
+//   const [message, setMessage] = useState("");
+//   const [chatMessages, setChatMessages] = useState([]);
+
+//   const messagesEndRef = useRef(null);
+
+//   const handleInputChange = (event) => {
+//     setMessage(event.target.value);
+//   };
+
+//   const handleSendMessage = () => {
+//     if (message.trim() !== "") {
+//       const newMessage = {
+//         user: "Agent007",
+//         text: message.trim(),
+//         timestamp: new Date().toLocaleString()
+//       };
+//       setChatMessages((prevMessages) => [...prevMessages, newMessage]);
+//       setMessage("");
+//     }
+//   };
+
+//   const handleKeyDown = (event) => {
+//     if (event.key === "Enter") {
+//       event.preventDefault();
+//       handleSendMessage();
+//     }
+//   };
+
+//   useEffect(() => {
+//     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+//   }, [chatMessages]);
+
+//   return (
+//     <>
+//       <TopBar />
+//       <div className="chat-page">
+//         <h2>Chat, not GPT4</h2>
+//         <div className="chat-container">
+//           {chatMessages.map((msg, index) => (
+//             <div key={index} className="chat-message">
+//               <strong>{msg.user}: </strong>
+//               {msg.text}
+//               <span className="timestamp">{msg.timestamp}</span>
+//             </div>
+//           ))}
+//           <div ref={messagesEndRef} />
+//         </div>
+//         <div className="chat-input">
+//           <input
+//             type="text"
+//             placeholder="Type here..."
+//             value={message}
+//             onChange={handleInputChange}
+//             onKeyDown={handleKeyDown}
+//           />
+//           <button onClick={handleSendMessage}>
+//             <span>Send</span>
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+const Token = localStorage.getItem("token");
 
 export const Chat = () => {
-  const [message, setMessage] = useState("");
-  const [chatMessages, setChatMessages] = useState([]);
-
-  const messagesEndRef = useRef(null);
-
-  const handleInputChange = (event) => {
-    setMessage(event.target.value);
-  };
-
-  const handleSendMessage = () => {
-    if (message.trim() !== "") {
-      const newMessage = {
-        user: "Agent007",
-        text: message.trim(),
-        timestamp: new Date().toLocaleString()
-      };
-      setChatMessages((prevMessages) => [...prevMessages, newMessage]);
-      setMessage("");
-    }
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      handleSendMessage();
-    }
-  };
-
   useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [chatMessages]);
+    const Token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhc2RAbzIucGwiLCJpYXQiOjE2ODczNTc3MDEsImV4cCI6MTY4NzM5MzcwMX0.5m-BsXzXL4rFIdsks1lfg78eeOkSGRMOv44ds6XlFWM';
+
+    const socket = io.connect("https://project-rest-api-production.up.railway.app/chat", {
+      transports: ['websocket'],
+      extraHeaders: {
+        Authorization: `Bearer ${Token}`,
+      }
+    });
+
+    // Obsługa zdarzeń socket.io
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    socket.on('message', (message) => {
+      console.log('Received message:', message);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
-    <>
-      <TopBar />
-      <div className="chat-page">
-        <h2>Chat, not GPT4</h2>
-        <div className="chat-container">
-          {chatMessages.map((msg, index) => (
-            <div key={index} className="chat-message">
-              <strong>{msg.user}: </strong>
-              {msg.text}
-              <span className="timestamp">{msg.timestamp}</span>
-            </div>
-          ))}
-          <div ref={messagesEndRef} />
-        </div>
-        <div className="chat-input">
-          <input
-            type="text"
-            placeholder="Type here..."
-            value={message}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
-          />
-          <button onClick={handleSendMessage}>
-            <span>Send</span>
-          </button>
-        </div>
-      </div>
-    </>
+    <div>
+      {/* Komponenty interfejsu użytkownika */}
+    </div>
   );
 };
--- src/page/Show.js
+++ src/page/Show.js
@@ -1,75 +1,214 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import TopBar from './TopBar';
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import TopBar from "./TopBar";
 import axios from "axios";
-import projects from './projectData';
-import '../styles/Show.css';
+import "../styles/Show.css";
 
 export const Show = () => {
+  const navigate = useNavigate();
   const Token = localStorage.getItem("token");
   const { id } = useParams();
   const [project, setProject] = useState(null);
+  const [users, setUsers] = useState({});
+  const [coopUsers, setCoopUsers] = useState([]);
+  const [userToAdd, setUserToAdd] = useState({});
+  const [showAddUser, setShowAddUser] = useState(false);
+  const [showAddTask, setShowAddTask] = useState(false);
 
   const fetchData = async () => {
+    // project
+    axios({
+      url: `https://project-rest-api-production.up.railway.app/project/${id}`,
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Token}`,
+      },
+    })
+      .then((res) => {
+        setProject(res.data);
+        //console.log(res.data);
+      })
+      .catch((err) => console.log(err));
 
-		axios({
-			url: `https://project-rest-api-production.up.railway.app/project/${id}`,
-			method: 'get',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${Token}`,
-			},
-		})
-			.then((res) => {
-				setProject(res.data)
-			})
-			.catch((err) => console.log(err))
-	}
+    // all users
+    axios({
+      url: `https://project-rest-api-production.up.railway.app/usersToProject`,
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Token}`,
+      },
+    })
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => console.log(err));
+
+    // users coop project
+    axios({
+      url: `https://project-rest-api-production.up.railway.app/projectCoop/${id}`,
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Token}`,
+      },
+    })
+      .then((res) => {
+        setCoopUsers(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  const handleAddUser = () => {
+  const handleShowAddUser = () => {
+    setShowAddUser(!showAddUser);
+    showAddTask === true ? setShowAddTask(false) : <></>;
+  };
 
-  }
+  const handleShowAddTask = () => {
+    setShowAddTask(!showAddTask);
+    showAddUser === true ? setShowAddUser(false) : <></>;
+  };
+
+  const handleAddUser = () => {
+    axios({
+      url: `https://project-rest-api-production.up.railway.app/addCoopUser`,
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Token}`,
+      },
+      data: {
+        user: { id: userToAdd },
+        projekt: { id: id },
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        setShowAddUser(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleFinishTask = () => {
+    axios({
+      url: `https://project-rest-api-production.up.railway.app/endProject/${id}`,
+      method: "patch",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        navigate(-1);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-
-      <div><TopBar /></div>
+      <div>
+        <TopBar />
+      </div>
       <div className="show-page">
-        <h2>{project && project.nazwa !== '' ? 'Show Project' : 'Project Not Found'}</h2>
+        <h2>
+          {project && project.nazwa !== ""
+            ? "Show Project"
+            : "Project Not Found"}
+        </h2>
         {project && (
-          <table>
-            <tbody>
-              <tr>
-                <td>ID:</td>
-                <td>{project.id}</td>
-              </tr>
-              <tr>
-                <td>Nazwa:</td>
-                <td>{project.nazwa}</td>
-              </tr>
-              <tr>
-                <td>Opis:</td>
-                <td>{project.opis}</td>
-              </tr>
-              <tr>
-                <td>Data Utworzenia:</td>
-                <td>{project.dataUtworzenia}</td>
-              </tr>
-              <tr>
-                <td>Lista członków:</td>
-                <td>
-                  
-                </td>
-              </tr>
-            </tbody>
-          </table>   
+          <>
+            <table>
+              <tbody>
+                <tr>
+                  <td>ID:</td>
+                  <td>{project.id}</td>
+                </tr>
+                <tr>
+                  <td>Nazwa:</td>
+                  <td>{project.nazwa}</td>
+                </tr>
+                <tr>
+                  <td>Opis:</td>
+                  <td>{project.opis}</td>
+                </tr>
+                <tr>
+                  <td>Data Utworzenia:</td>
+                  <td>{project.dataUtworzenia}</td>
+                </tr>
+                <tr>
+                  <td>Lista członków:</td>
+                  <td>
+                    {coopUsers.map((user) => (
+                      <>
+                        <div>{user.email}</div>
+                      </>
+                    ))}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Zadania:</td>
+                  <td>z1</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div className="btn-container">
+              <button
+                className="btn-add-users"
+                onClick={() => handleShowAddUser()}
+              >
+                dodaj członków
+              </button>
+              <button
+                className="btn-add-users"
+                onClick={() => handleShowAddTask()}
+              >
+                dodaj zadanie
+              </button>
+              <button className="btn-finish" onClick={() => handleFinishTask()}>
+                zakoncz
+              </button>
+            </div>
+
+            {showAddUser === true ? (
+              <div className="add-user-container">
+                {users.map((user) => (
+                  <label key={user.id} className="radio-label">
+                    <input
+                      type="radio"
+                      className="radio-input"
+                      onChange={() => setUserToAdd(user.id)}
+                    />
+                    {user.email}
+                  </label>
+                ))}
+
+                <button onClick={() => handleAddUser()}>zapisz</button>
+              </div>
+            ) : (
+              <></>
+            )}
+
+            {showAddTask === true ? (
+              <div className="add-user-container">
+                <input type="text"/>
+              </div>
+            ) : (
+              <></>
+            )}
+          </>
         )}
-        <button onClick={handleAddUser} className='btn-add-users'>dodaj członków</button>
       </div>
     </>
   );
